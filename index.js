9e9f79d968c01ccb8400a4f03b5a8d89142aac65
Kirusha28 <62811727+Kirusha28@users.noreply.github.com>
2023-10-04 19:05:51 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const el = document.getElementById('btn-play');
 const playing = false;
 const player = new Audio('gg.mp3');
 player.preload = "auto";
-player.addEventListener('ended', function(){ // слушаем окончание трека
-  el.innerText = "Done";
-  playing = false;
+el.addEventListener('click', () => {
+    player.play();
+    console.log("gg");
 });
-el.addEventListener('click', () => {player.play();});
